a9ee9d29728f8e776cec483a52e2ac6e461cc898
scign <scignspam.github@mailnull.com>
2020-07-23 14:42:20 -0400

    added author, better layout, added code comments

--- app/quotes.py
+++ app/quotes.py
@@ -5,12 +5,14 @@ from app import app
 import pandas as pd
 import gensim.downloader as api
 
+# sometimes the model needs a couple of tries to load
 while True:
     try:
         model = api.load('glove-wiki-gigaword-50')
         break
     except:
         pass
+# read in the quotes database
 q = pd.read_csv(os.path.join('app','data','quotes_all.csv'), sep=';', skiprows=1, header=0)
 
 # there are a few quote genres that don't occur in the model vocab
@@ -21,12 +23,15 @@ replace = {
     'memorialday': 'memorial',
     'mothersday': 'mothers',
     'newyears': 'year',
-    'saintpatricksday': 'patrick',
+    'saintpatricksday': 'ireland',
     'valentinesday': 'valentine'
 }
 q['GENRE'].replace(to_replace=replace, inplace=True)
 
 def get_random_word():
+    '''
+    Returns a random category label from the data
+    '''
     random_word = q['GENRE'].sample(1).iloc[0]
     return random_word
 
@@ -43,12 +48,16 @@ def get_closest_words(word, choices, n=1):
     '''
     app.logger.info(f'Finding closest words to "{word}"')
     if word in choices:
+        # if the word is already in the list return the same word with 100% match
         return [(word, 1.0)]
     if word in model.vocab.keys():
+        # if not in the list, find the closest words
         similarities = [(choice, model.similarity(word, choice)) for choice in choices]
+        # sort, reverse, and return the top n (word,similarity) tuples
         return sorted(similarities, key=lambda x: x[1])[::-1][:n]
     else:
         app.logger.info(f'Not in model vocab: "{word}"')
+        # if the requested label isn't in the model vocab, return a random genre
         return [(get_random_word(), 1.0), (word, 0.0)]
 
 def find_matching_quote(genre):
@@ -59,10 +68,17 @@ def find_matching_quote(genre):
 
     Returns:
     (str) Quote
+    (str) Author
     (list) List of tuples in the form (word (str), simliarity (float))
     '''
+    # find closest matches
     matched_genres = get_closest_words(genre, q.GENRE.unique(), 5)
+    # get the best one
     closest = matched_genres[0][0]
     app.logger.info(f'Finding quote for: "{closest}"')
-    matching_quote = q['QUOTE'][q['GENRE']==closest].sample(1).iloc[0]
-    return matching_quote, matched_genres
+    # get a quote from that genre
+    matching_quote = q[q['GENRE']==closest].sample(1).iloc[0]
+    quote = matching_quote.QUOTE
+    author = matching_quote.AUTHOR
+    # return the quote and the genres
+    return quote, author, matched_genres
--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,22 @@ from app.quotes import get_random_word, find_matching_quote
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    # generate 4 random words
     genres = [get_random_word() for _ in range(4)]
+    
+    # if no requested word, pick the first random word
     genre = request.args.get('genre', default=genres[0])
     app.logger.info(f'Request received: "{genre}"')
-    quote, matches = find_matching_quote(genre)
-    return render_template('quote_finder.html', genres=genres, quote=quote, matches=matches)
+    
+    # find matching quote
+    quote, author, matches = find_matching_quote(genre)
+    
+    # display the page
+    return render_template(
+        'quote_finder.html',
+        requested=genre,
+        genres=genres,
+        quote=quote,
+        author=author,
+        matches=matches
+    )
